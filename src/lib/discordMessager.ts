271bb994b2f11bbe00c9a131b448e4a79a1e20ff
Tolfx <matteus12122@gmail.com>
2020-09-07 15:56:03 +0200

    ✨Updated < discordMessager >

--- src/lib/discordMessager.ts
+++ src/lib/discordMessager.ts
@@ -1,25 +1,25 @@
-import request from "request";
-import config from "../../config.json";
+import request from 'request';
+import config from '../../config.json';
 
 export default function sendMessage(serverID, Message) {
   let data = {
-    "content": Message,
-    "nonce": "751905390169223872", // <-- Needs some more research.
-    "tts": false,
+    content: Message,
+    tts: false,
   };
 
   request(
     {
       url: `https://discord.com/api/v8/channels/${serverID}/messages`,
-      method: "POST",
+      method: 'POST',
       headers: {
-        authorization: config.Discord.authToken,
-        "content-type": "application/json",
+        client_secret: config.General.tokenDiscord,
+        'content-type': 'application/json',
       },
       body: JSON.stringify(data),
     },
     (err, response, body) => {
       if (err) throw err;
-    },
+      console.log(body);
+    }
   );
 }
