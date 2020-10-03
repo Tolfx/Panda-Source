10b670fcbd701160d6abd2e72a82c1db61fcd1c8
tres <59184373+pybash@users.noreply.github.com>
2020-10-02 22:29:33 -0400

    wantsBigCommIcons boolean

--- src/events/checkBan.ts
+++ src/events/checkBan.ts
@@ -16,7 +16,6 @@ import { stalkGold } from '../events/stalkGold';
 
 let D_ID = getID(config.Discord.NewBans);
 let D_Token = getToken(config.Discord.NewBans);
-let wantsBigIcons = config.Boolean.Wants_Big_Icons;
 
 const path = paths.NewBan;
 const goldkek = new stalkGold();
