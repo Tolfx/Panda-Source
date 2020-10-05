1bf5f78acee89241ac08c8aba8936eab29255ff3
Tolfx <matteus12122@gmail.com>
2020-10-05 19:15:33 +0200

    🎈Fixed < getUser.ts >

--- src/commands/steam/getUser.ts
+++ src/commands/steam/getUser.ts
@@ -12,110 +12,63 @@ export class getUserSteam {
   private start(client, message, args) {
     if (!args[0]) return;
 
-    let embed = new MessageEmbed().setColor(0x9c98d9);
-    let reg = new RegExp('^[0-9]+$');
-    if (args[0].includes('STEAM_') || args[0].includes('[U:') || typeof Number) {
-      let steamID64 = this.getID64(args[0]);
-      this.getInfo64(steamID64, (err, result) => {
-        if (err) return message.channel.send(`${err}`);
-        try {
-          let sid = new steamID(result.steamID64);
+    let embed = new MessageEmbed()
+    .setColor(0x9c98d9);
 
-          message.channel.send(
-            embed.setAuthor(result.custom, result.avatar).setDescription(stripIndents`
-                    **SteamID:** ${sid.getSteam2RenderedID()}
-                    **SteamID32:** ${sid.getSteam3RenderedID()}
-                    **SteamID64:** ${sid.getSteamID64()}
-                    
-                    **Privacy:** ${result.privacy}
-                    **Joined:** ${result.joined}
-                    **Status:** ${result.onlineState}`)
-          );
-        } catch (err) {
-          message.channel.send('Error: ' + err);
-        }
+    this.getInfo(args[0], (err, result) => {
+      if(err)
+        return message.channel.send("Error: "+err)
+
+      let sid = new steamID(result.steamID64);
+      message.channel.send(
+        embed
+        .setAuthor(result.custom, result.avatar)
+        .setDescription(stripIndents`
+          **SteamID:** ${sid.getSteam2RenderedID()}
+          **SteamID32:** ${sid.getSteam3RenderedID()}
+          **SteamID64:** ${sid.getSteamID64()}
+          
+          **Privacy:** ${result.privacy}
+          **Joined:** ${result.joined}
+          **Status:** ${result.onlineState}`)
+      );
+    });
+  }
+
+  private getInfo(info: string, callback?) {
+    let pushURL = [];
+
+    if (info.match(/STEAM_[0-9]:[0-9]:[0-9]{7}/g)) {
+      pushURL.push({
+        url: `https://steamcommunity.com/profiles/${this.getID64(info)}/?xml=1`,
       });
-    } else if (args[0].includes('https://steamcommunity.com/')) {
-      this.getInfoSteamURL(args[0], (err, result) => {
-        if (err) return message.channel.send(`${err}`);
-        try {
-          let sid = new steamID(result.steamID64);
-
-          message.channel.send(
-            embed.setAuthor(result.custom, result.avatar).setDescription(stripIndents`
-                    **SteamID:** ${sid.getSteam2RenderedID()}
-                    **SteamID32:** ${sid.getSteam3RenderedID()}
-                    **SteamID64:** ${sid.getSteamID64()}
-                    
-                    **Privacy:** ${result.privacy}
-                    **Joined:** ${result.joined}
-                    **Status:** ${result.onlineState}`)
-          );
-        } catch (err) {
-          message.channel.send('Error: ' + err);
-        }
+    } else if (info.includes('https://steamcommunity.com/')) {
+      pushURL.push({
+        url: info+"/?xml=1",
       });
-    } else {
-      this.getInfoCutomURL(args[0], (err, result) => {
-        if (err) return message.channel.send(`${err}`);
-        try {
-          let sid = new steamID(result.steamID64);
-
-          message.channel.send(
-            embed.setAuthor(result.custom, result.avatar).setDescription(stripIndents`
-                    **SteamID:** ${sid.getSteam2RenderedID()}
-                    **SteamID32:** ${sid.getSteam3RenderedID()}
-                    **SteamID64:** ${sid.getSteamID64()}
-                    
-                    **Privacy:** ${result.privacy}
-                    **Joined:** ${result.joined}
-                    **Status:** ${result.onlineState}`)
-          );
-        } catch (err) {
-          message.channel.send('Error: ' + err);
-        }
+    } else if (!info.match(/\w/g)) {
+      pushURL.push({
+        url: `https://steamcommunity.com/profiles/${this.getID64(info)}/?xml=1`,
+      });
+    } else if (info.match(/[0-9]{1}/g)) {
+      pushURL.push({
+        url: `https://steamcommunity.com/profiles/${this.getID64(info)}/?xml=1`,
+      });
+    } else if (info.match(/\[U:[0-9]:[0-9]{9}]/g)) {
+      pushURL.push({
+        url: `https://steamcommunity.com/profiles/${this.getID64(info)}/?xml=1`,
+      });
+    } else if (info.match(/\w/g)) {
+      pushURL.push({
+        url: `https://steamcommunity.com/id/${info}/?xml=1`,
       });
     }
-  }
 
-  private getInfoCutomURL(customURL, callback: (err: null | Error, result?: any) => void) {
+    console.log(pushURL[0].url)
+
     request(
       {
-        url: `https://steamcommunity.com/id/${customURL}/?xml=1`,
-        headers: { 'Content-Type': 'text/xml' },
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          parseString(body, (err, result) => {
-            if (result.response === undefined) {
-              let steamID64 = result.profile.steamID64[0];
-              let avatar = result.profile.avatarMedium[0];
-              let privacy = result.profile.privacyState[0];
-              let custom = result.profile.customURL[0];
-              let joined = result.profile.memberSince[0];
-              let onlineState = result.profile.onlineState[0];
-
-              callback(null, {
-                steamID64,
-                avatar,
-                privacy,
-                custom,
-                joined,
-                onlineState,
-              });
-            } else {
-              return callback(new Error('No matching CustomURL'));
-            }
-          });
-        }
-      }
-    );
-  }
-
-  private getInfo64(steamid64, callback: (err: null | Error, result?) => void) {
-    request(
-      {
-        url: `https://steamcommunity.com/profiles/${steamid64}/?xml=1`,
+        url: `${pushURL[0].url}`,
         headers: { 'Content-Type': 'text/xml' },
       },
       (error, response, body) => {
@@ -139,40 +92,6 @@ export class getUserSteam {
                 joined,
                 onlineState,
               });
-            }
-          });
-        }
-      }
-    );
-  }
-
-  private getInfoSteamURL(url, callback: (err: null | Error, result?) => void) {
-    request(
-      {
-        url: `${url}?xml=1`,
-        headers: { 'Content-Type': 'text/xml' },
-      },
-      (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-          parseString(body, (err, result) => {
-            if (!result.response === undefined) {
-              let steamID64 = result.profile.steamID64[0];
-              let avatar = result.profile.avatarMedium[0];
-              let privacy = result.profile.privacyState[0];
-              let custom = result.profile.customURL[0];
-              let joined = result.profile.memberSince[0];
-              let onlineState = result.profile.onlineState[0];
-
-              callback(null, {
-                steamID64,
-                avatar,
-                privacy,
-                custom,
-                joined,
-                onlineState,
-              });
-            } else {
-              return callback(new Error('No matching URL'));
             }
           });
         }
